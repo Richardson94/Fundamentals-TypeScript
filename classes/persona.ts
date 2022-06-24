1c122751b43472185d0a55416ecdb5e18771f3f7
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-06-24 19:37:31 -0400

    Richard
    test typescript files

---
+++ classes/persona.ts
@@ -0,0 +1,14 @@
+export class Persona{
+
+    Name:String;
+    LastName:String;
+    Age:Number;
+
+    constructor(Name:String, LastName:String, Age:Number){
+        this.Name = Name;
+        this.LastName = LastName;
+        this.Age = Age;
+    }
+    
+
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+let persona ={
+    nombre: ' Nelson',
+    edad: 28,
+    direccion:{
+        calle: 'octavio campero',
+        zona: 'Antofagasta'
+    }
+}
+
+ console.log(persona);
