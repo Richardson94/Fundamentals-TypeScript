beb575da5794fad75b47d65864010154d4c62c14
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-06-30 20:09:16 -0400

    RichardSon94
    merge and changes with Eslint

--- queues/index.ts
+++ queues/index.ts
@@ -1,6 +1,6 @@
-import { Queues } from "./queue";
+import { Queues } from './queue';
 
-const maxSize =3;
+const maxSize = 3;
 const queue = new Queues(maxSize);
 
 queue.enqueue(2);
@@ -8,6 +8,6 @@ queue.enqueue(3);
 queue.enqueue(4);
 
 const deleteValue = queue.dequeue();
-console.log(`value to delete is: ${deleteValue}`)
+console.log(`value to delete is: ${deleteValue}`);
 
 queue.printQueue();
