efa4777137c585e57913b421021ab4b56c43936c
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-07-06 21:28:02 -0400

    RCS
    practice with callbacks

---
+++ callback/example.js
@@ -0,0 +1,12 @@
+function modify (array,callback) {
+  array.push('prueba');
+  setTimeout(function () {
+    callback(array);
+  }, 3000);
+}
+
+const nombres = ['Nelson', 'Richard', 'Cori', 'Sirpa'];
+
+modify(nombres, function (array) {
+  console.log(`He modificado el Array ahora tiene ${array.length}`);
+});
