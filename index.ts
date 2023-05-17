070da6ae8adf264727ec6e7da8c2ea3b03527dfc
Richardson94 <richcori94@gmail.com>
2023-05-16 22:47:39 -0400

    adding test in typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hola Mundo with TypeScript");
+const name: string = 'Strinder';
+let hpPoints: number | 'FULL' = 95;
+const isAlive: boolean = false;
+
+hpPoints = 'FULL';
+
+console.log(name, hpPoints, isAlive);
+export {};
